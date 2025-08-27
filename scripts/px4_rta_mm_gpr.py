892637a4df4ad723d7ab9608ee3aa3d80d6a2d00
evannsm <evannsmcuadrado@gmail.com>
2025-08-26 20:44:14 -0400

    edit: remove shutdown hooks from ROS2Logger to avoid shutdown stalling. still need to experiment with Q_ref_planar weights

--- scripts/px4_rta_mm_gpr.py
+++ scripts/px4_rta_mm_gpr.py
@@ -5,8 +5,6 @@ import traceback
 
 import rclpy # Import ROS2 Python client library
 from .rta_mm_gpr_node import OffboardControl
-from Logger import Logger, install_shutdown_logging # pyright: ignore[reportMissingImports]
-
 
 BANNER = "=" * 65
 print(f"{BANNER}\nInitializing ROS 2 node\n{BANNER}")
@@ -30,22 +28,20 @@ def main():
     rclpy.init()
     offboard_control = OffboardControl(sim)
 
-    base_path = os.path.dirname(os.path.abspath(__file__))  # Get the script's directory
-    log = Logger(filename, base_path)
+    # base_path = os.path.dirname(os.path.abspath(__file__))  # Get the script's directory
+    # log = Logger(filename, base_path)
 
-    install_shutdown_logging(log, offboard_control)#, also_shutdown=rclpy.shutdown)# Ensure logs are flushed on Ctrl+C / SIGTERM / normal exit
+    # install_shutdown_logging(log, offboard_control)#, also_shutdown=rclpy.shutdown)# Ensure logs are flushed on Ctrl+C / SIGTERM / normal exit
     try:
         rclpy.spin(offboard_control)
     except KeyboardInterrupt:
         print(
-              f"\n{65 * '='}\n"
-              f"Keyboard interrupt detected (Ctrl+C), exiting...\n"
-              )
+              f"{BANNER}\nKeyboard interrupt detected (Ctrl+C), exiting...{BANNER}")
     except Exception as e:
         traceback.print_exc()
     finally:
-        # belt & suspenders
-        pass
+        offboard_control.destroy_node()
+        rclpy.shutdown()
 
 if __name__ == '__main__':
     main()
