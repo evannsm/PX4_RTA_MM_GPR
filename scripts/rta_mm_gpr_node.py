9edf58236851457636e449e1c5bed6a5368e1217
evannsm <evannsmcuadrado@gmail.com>
2025-11-14 18:21:33 -0500

    9::29::3 for logging ref and tube

--- scripts/rta_mm_gpr_node.py
+++ scripts/rta_mm_gpr_node.py
@@ -77,9 +77,9 @@ class OffboardControl(Node):
 
         self.tube_pos_indices = [0, 1, 5, 6]  # Indices for x, y, z, yaw in the rollout reference trajectory
 
-        self.tube_start = 10
-        self.tube_extent = 35
-        self.tube_skip = 4
+        self.tube_start = 9
+        self.tube_extent = 29
+        self.tube_skip = 3
 
         tube_start = self.tube_start
         tube_end = self.tube_start + self.tube_extent
